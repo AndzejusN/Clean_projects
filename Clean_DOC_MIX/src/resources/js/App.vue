c8bdb643848aef1a56409189bc8048ea1be6e8d6
Andrew <90209255+AndzejusN@users.noreply.github.com>
2022-05-06 15:46:13 +0300

    2-nd Clean project added

---
+++ Clean_DOC_MIX/src/resources/js/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+import Test from './components/sidebars/ExampleComponent';
+
+</script>
+
+<template>
+
+    <div class="container py-3">
+        <main class="py-3">
+            <h1>Clean project</h1>
+            <Test/>
+        </main>
+    </div>
+
+</template>
